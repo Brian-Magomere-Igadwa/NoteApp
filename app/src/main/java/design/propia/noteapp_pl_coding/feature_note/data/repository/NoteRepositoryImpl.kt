1620f2229c2946165d8fef04688d5f9950e105f1
Igadwa Brian <igadwabrian7@gmail.com>
2023-01-22 16:15:56 +0300

    Associate the domain layer to the data layer through an interface..

---
+++ app/src/main/java/design/propia/noteapp_pl_coding/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package design.propia.noteapp_pl_coding.feature_note.data.repository
+
+import design.propia.noteapp_pl_coding.feature_note.data.data_source.NoteDao
+import design.propia.noteapp_pl_coding.feature_note.domain.models.Note
+import design.propia.noteapp_pl_coding.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl(
+    private val dao: NoteDao
+):NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        return dao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return dao.getNoteById(id)
+    }
+
+    override suspend fun insertNote(note: Note) {
+        dao.insertNote()
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        dao.deleteNote(note)
+    }
+}
